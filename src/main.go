0693e9aea3716302097a73bf553c650cc9969017
Michel Zuidema <michelgzuidema@gmail.com>
2021-10-21 22:01:58 +0200

    Added update and delete functionality

--- src/main.go
+++ src/main.go
@@ -7,11 +7,12 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+
 	"github.com/gorilla/mux"
 )
 
 type book struct {
-	Id          int `json:"id"`
+	Id          int    `json:"id"`
 	Title       string `json:"title"`
 	Author      string `json:"author"`
 	Description string `json:"description"`
@@ -43,6 +44,53 @@ func createBook(rw http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(rw).Encode(newBook)
 }
 
+func updateBook(rw http.ResponseWriter, r *http.Request) {
+	bid := mux.Vars(r)["id"]
+	var newBook book
+
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(rw, "No input")
+	}
+
+	json.Unmarshal(reqBody, &newBook)
+
+	for i, b := range books {
+		bookId, err := strconv.Atoi(bid)
+
+		if err != nil {
+			fmt.Fprintf(rw, "Something went wrong")
+		}
+
+		if b.Id == bookId {
+			b.Title = newBook.Title
+			b.Description = newBook.Description
+			b.Author = newBook.Author
+
+			books = append(books[:i], b)
+
+			json.NewEncoder(rw).Encode(b)
+		}
+	}
+}
+
+func deleteBook(rw http.ResponseWriter, r *http.Request) {
+	bid := mux.Vars(r)["id"]
+
+	for i, b := range books {
+		bookId, err := strconv.Atoi(bid)
+
+		if err != nil {
+			fmt.Fprintf(rw, "Something went wrong")
+		}
+
+		if b.Id == bookId {
+			books = append(books[:i], books[i+1:]...)
+			fmt.Fprintf(rw, "The book with ID %v has been deleted successfully!", bookId)
+		}
+	}
+}
+
 func getBookById(rw http.ResponseWriter, r *http.Request) {
 	id := mux.Vars(r)["id"]
 
@@ -57,6 +105,10 @@ func getBookById(rw http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func getAllBooks(rw http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(rw).Encode(books)
+}
+
 func welcome(rw http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(rw, "Homescreen")
 }
@@ -68,8 +120,12 @@ func main() {
 	fmt.Println("Listening on port 8080")
 
 	router.HandleFunc("/", welcome).Methods("GET")
-	router.HandleFunc("/addBook", createBook).Methods("POST")
+
+	router.HandleFunc("/books", getAllBooks).Methods("GET")
 	router.HandleFunc("/book/{id}", getBookById).Methods("GET")
+	router.HandleFunc("/book/{id}", updateBook).Methods("PATCH")
+	router.HandleFunc("/book/{id}", deleteBook).Methods("DELETE")
+	router.HandleFunc("/addBook", createBook).Methods("POST")
 
 	log.Fatal(http.ListenAndServe(fmt.Sprint(":", port), router))
 }
